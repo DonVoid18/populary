4c6c6e8e20eab0fe9f87ce18d5d48cb7fc2c9f71
AngeloPatrick <angelopatrickriosnolasco@gmail.com>
2022-03-04 01:25:05 -0500

    optimization of page styles

--- php/main.php
+++ php/main.php
@@ -1,26 +1,34 @@
 </head>
 
 <body>
-        <div class="app">
+        <div id="root">
                 <main id="main">
-                        <div class="container_video">
-                                <img id="logo" src="<?php echo $link_base_root ?>/logo/logo_populary.png" alt="Logo Populary" title="Populary">
-                                <video id="video" autoplay="true"></video>
-                                <button id="button_capture">Capturar Fotografía</button>
-                                <div class="container_answer_status">
-                                        <p id="status_fetchApi"></p>
+                        <section class="container_section_principal">
+                                <div class="container_logo_page">
+                                        <img id="logo" src="<?php echo $link_base_root ?>/logo/logo_populary.png" alt="Logo Populary" title="Populary">
                                 </div>
-                                <?php
-                                include("./save_images/icon_preloader.php");
-                                ?>
+
+                                <div class="container_video">
+                                        <video id="video" autoplay="true"></video>
+                                        <button id="button_capture">Capturar Fotografía</button>
+                                        <div class="container_answer_status">
+                                                <p id="status_fetchApi"></p>
+                                        </div>
+
+                                        <?php
+                                        include("./save_images/icon_preloader.php");
+                                        ?>
+
+                                        <div class="container_canvas">
+                                                <canvas id="canvas" style="display: none"></canvas>
+                                        </div>
+                                </div>
+
                                 <div class="container_link_images">
                                         <a class="item_link_images" href="<?php echo $link_base_root ?>/images_users/images.php">
                                                 Todas las imágenes
                                         </a>
                                 </div>
-                                <div class="container_canvas">
-                                        <canvas id="canvas" style="display: none"></canvas>
-                                </div>
-                        </div>
+                        </section>
                 </main>
         </div>
